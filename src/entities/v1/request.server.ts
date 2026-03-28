5e65e76ab92445291ace43e2fe1fee1097bbe7aa
ryotaKFC <ryotamokatoka@gmail.com>
2026-03-28 21:46:07 +0900

    refactor: projectのほうを再利用

--- src/entities/v1/request.server.ts
+++ src/entities/v1/request.server.ts
@@ -1,22 +1,12 @@
 import "server-only";
 import { firestore } from "firebase-admin";
 import v from "@/entities/valibot";
-import { Project } from "./project";
 import { Request } from "./request";
-
-// Request エンティティの createdAt を Firestore の Timestamp 型で扱うバージョン
-// Timestamp が Server SDK 特有の型であるため分ける
-const ProjectInRequestWithTimestamp = v.object({
-  ...v.omit(Project, ["createdAt"]).entries,
-  createdAt: v.custom<firestore.Timestamp>(
-    (data) => data instanceof firestore.Timestamp,
-    "Expected firestore.Timestamp",
-  ),
-});
+import { ProjectWithTimestamp } from "./project.server";
 
 export const RequestWithTimestamp = v.object({
   ...v.omit(Request, ["project", "date", "createdAt"]).entries,
-  project: ProjectInRequestWithTimestamp,
+  project: ProjectWithTimestamp,
   date: v.custom<firestore.Timestamp>(
     (data) => data instanceof firestore.Timestamp,
     "Expected firestore.Timestamp",
